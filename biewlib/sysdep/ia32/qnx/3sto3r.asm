815ff0d7c96a40dab6651718e39a6af7a5ca9892
andrew_golovnia <>
2003-07-24 08:08:06 +0000

    Add file: 3s->3r calls for C functions

---
+++ biewlib/sysdep/ia32/qnx/3sto3r.asm
@@ -0,0 +1,52 @@
+; **
+; * @namespace   biewlib
+; * @file        biewlib/sysdep/ia32/qnx/3rto3s.asm
+; * @brief       This file contains 3s->3r conversion function for photon3s.lib
+; * @version     -
+; * @remark      this source file is part of Binary vIEW project (BIEW).
+; *              The Binary vIEW (BIEW) is copyright (C) 1995 Nick Kurshev.
+; *              All rights reserved. This software is redistributable under the
+; *              licence given in the file "Licence.en" ("Licence.ru" in russian
+; *              translation) distributed in the BIEW archive.
+; * @note        Requires POSIX compatible development system
+; * @author      Andrew Golovnia
+; * @since       2002
+; * @note        Development, fixes and improvements
+;**
+
+.386p
+.387
+.model flat
+.code
+
+;	--------------------------------------------------------------------------
+;	Open a communications channel
+;	struct _Ph_ctrl *PhAttach(char const *name,PhChannelParms_t const *parms);
+;	--------------------------------------------------------------------------
+extern PhAttach_ :near
+public PhAttach
+PhAttach proc near
+	push	edx
+	mov		eax,08h[esp]
+	mov		edx,0ch[esp]
+	call	PhAttach_
+	pop		edx
+	ret
+PhAttach endp
+
+;	--------------------------------------------------------------------------
+;	Collect cursor information
+;	int PhQueryCursor(unsigned short ig,PhCursorInfo_t *buf);
+;	--------------------------------------------------------------------------
+extern PhQueryCursor_ :near
+public PhQueryCursor
+PhQueryCursor proc near
+	push	edx
+	mov		eax,08h[esp]
+	mov		edx,0ch[esp]
+	call	PhQueryCursor_
+	pop		edx
+	ret
+PhQueryCursor endp
+
+end
