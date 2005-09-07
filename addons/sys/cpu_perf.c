7f667d2fa23b9e20549d8db1850c5b525284df60
andrew_golovnia <>
2005-09-07 18:29:03 +0000

    CPUInfo display window changed to scrollable window

--- addons/sys/cpu_perf.c
+++ addons/sys/cpu_perf.c
@@ -13,7 +13,14 @@
  * @author      Nick Kurshev
  * @since       1995
  * @note        Development, fixes and improvements
+ * @author      Andrew Golovnya <andrew_golovnia at users dot sourceforge dot net>
+ * @note        CPUInfo display window changed to scrollable window
 **/
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
 #include "bconsole.h"
 #include "biewutil.h"
 #include "reg_form.h"
@@ -29,25 +36,50 @@ static void paint_prcnt(int n_prcnt)
   ShowPercentInWnd(pwnd,n_prcnt);
 }
 
+char ** __FASTCALL__ cpuPointStrings(char __HUGE__ *data,unsigned long data_size,unsigned long *nstr)
+{
+  char **str_ptr,**new_ptr;
+  unsigned long i;
+  char ch,ch1;
+  *nstr = 0;
+  if((str_ptr = PMalloc(sizeof(char *) * 2)) != NULL)
+  {
+     str_ptr[(*nstr)++] = &data[0];
+     for(i = 0;i < data_size;i++)
+     {
+       ch = data[i];
+       if(ch == '\n' || ch == '\r')
+       {
+         data[i] = 0;
+         if(!(new_ptr = PRealloc(str_ptr,((unsigned)(*nstr)+1)*sizeof(char *)))) goto mem_off;
+         str_ptr = new_ptr;
+         ch1 = data[i+1];
+         if((ch1 == '\n' || ch1 == '\r') && ch != ch1) ++i;
+         str_ptr[(*nstr)++] = &data[i+1];
+       }
+       if(*nstr > UINT_MAX-2) { mem_off: PFree(str_ptr); return NULL; }
+     }
+  }
+  return str_ptr;
+}
+
 static void ShowCPUInfo( void )
 {
-   TWindow *win;
    unsigned evt;
    char *cpu_info;
-   cpu_info = PMalloc(2048);
-   if(!cpu_info) { MemOutBox("Show CPU information"); return; }
-   win = CrtDlgWndnls(" CPU information ",78,22);
+   char **str_ptr;
+   unsigned long data_size;
+   unsigned long nstr;
+   cpu_info = PMalloc(4096);
+   if(!cpu_info) { mem_out: MemOutBox("Show CPU information"); return; }
    pwnd = PercentWnd("Analyze:","");
-   __FillCPUInfo(cpu_info,2048,paint_prcnt);
+   __FillCPUInfo(cpu_info,4096,paint_prcnt);
    CloseWnd(pwnd);
-   twGotoXY(1,1);
-   twPrintF("%s", cpu_info);
-   do
-   {
-     evt = GetEvent(drawEmptyPrompt,NULL,win);
-   }
-   while(!(evt == KE_ESCAPE || evt == KE_F(10)));
-   CloseWnd(win);
+   data_size = strchr(cpu_info, 0) - cpu_info;
+   if(!(str_ptr = cpuPointStrings(cpu_info,data_size,&nstr)))
+      { PFREE(cpu_info); goto mem_out; }
+   DisplayBox((const char**)str_ptr,(unsigned)nstr - 1," CPU information ");
+   PFREE(str_ptr);
    PFREE(cpu_info);
 }
 
