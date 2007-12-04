98f96a9f4ae9bf974e19aea6d5e138614a6874c7
nickols_k <>
2007-12-04 16:47:24 +0000

    be ready for next release

--- tstrings.h
+++ tstrings.h
@@ -20,7 +20,7 @@
 /** @def BIEW_VERSION
     Ver.SubVer.ExtraVer-ReleaseLevel.Test_FixPak_Level
   */
-#define BIEW_VERSION "5.6.4"
+#define BIEW_VERSION "5.7.0"
 
 extern const char   msgUndef[];
 extern const char   msgFatalError[];
